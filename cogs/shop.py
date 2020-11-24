278c1b3a9781ad2697ff365a52263964fdcda9a8
Kane Du <kanedu2000@gmail.com>
2020-11-24 05:37:44 -0500

    added more dbutil functions

--- cogs/shop.py
+++ cogs/shop.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import random
 from util.unit_directory import unit_list, Rarity
+import util.dbutil as db
 
 class Shop(commands.Cog):
 
@@ -10,19 +11,29 @@ class Shop(commands.Cog):
 
     @commands.command(name = 'pull')
     async def pull(self, ctx, amount=1):
+        message_embed = discord.Embed(title = 'Unit Pull!')
         def random_unit(rarity):
             possible_units = [key for key, value in unit_list.items() if value['rarity'] == rarity]
             pulled_unit_id = random.choice(possible_units)
             return pulled_unit_id
         rarity_odds = random.randint(0, 100)
-        if rarity_odds <= 10:
+        if rarity_odds <= 5:
             rarity = Rarity.LEGENDARY
-        elif rarity_odds <= 30:
+        elif rarity_odds <= 25:
             rarity = Rarity.EPIC
-        elif rarity_odds <= 100:
+        elif rarity_odds <= 75:
             rarity = Rarity.RARE
-        unit_name = unit_list[random_unit(rarity)]['unit'].name
-        await ctx.send(f"You got a {unit_name}")
+        else:
+            essence = random.randint(300, 1000)
+            await db.update_user_essence(ctx.author.id, essence)
+            message_embed.description = f'{ctx.author.mention} pulled {essence} essence!'
+            await ctx.send(embed = message_embed)
+            return
+        random_unit_id = random_unit(rarity)
+        unit_name = unit_list[random_unit_id]['unit'].name
+        await db.insert_unit(random_unit_id, ctx.author.id, -1)
+        message_embed.description = f'{ctx.author.mention} pulled a {rarity} unit! {unit_name}!'
+        await ctx.send(embed = message_embed)
 
 def setup(client):
     client.add_cog(Shop(client))
--- util/dbutil.py
+++ util/dbutil.py
@@ -6,20 +6,15 @@ load_dotenv()
 
 PSQL_CONNECTION_URL = os.getenv('PSQL_CONNECTION_URL')
 
-
-async def test():
-    conn = await asyncpg.connect(PSQL_CONNECTION_URL)
-    await conn.close()
-
 async def get_all_users():
     conn = await asyncpg.connect(PSQL_CONNECTION_URL)
     stmt = await conn.prepare("SELECT * FROM users WHERE user_id='124668192948748288'")
     print(await stmt.fetch())
     await conn.close()
 
-async def create_user(id: int):
+async def insert_user(id: int):
     conn = await asyncpg.connect(PSQL_CONNECTION_URL)
-    stmt = await conn.prepare("INSERT INTO users(user_id) VALUES ($1) RETURNING user_id, essence, currency, stage")
+    stmt = await conn.prepare("INSERT INTO users(user_id) VALUES ($1) RETURNING user_id, essence, currency, stage, elo")
     result = await stmt.fetch(id)
     await conn.close()
     return result
@@ -29,15 +24,95 @@ async def get_user(id: int):
     stmt = await conn.prepare("SELECT * FROM users WHERE user_id=$1")
     result = await stmt.fetch(id)
     if not result:
-        result = await create_user(id)
+        result = await insert_user(id)
     await conn.close()
     user_data = {}
     for field, value in result[0].items():
         user_data[field] = value
-    print(user_data)
     return user_data
 
-#TODO: add elo field to users, add position field to units
+async def insert_unit(unit_id: int, user_id: int, location: int):
+    await get_user(user_id)
+    unit = await get_unit(unit_id, user_id)
+    conn = await asyncpg.connect(PSQL_CONNECTION_URL)
+    if unit:
+        stmt = await conn.prepare("UPDATE units SET stars=stars + 1 WHERE unit_id=$1 AND user_id=$2 RETURNING unit_id, user_id, location, level, stars")
+        result = await stmt.fetch(unit_id, user_id)
+    else:
+        stmt = await conn.prepare("INSERT INTO units(unit_id, user_id, location) VALUES ($1, $2, $3) RETURNING unit_id, user_id, location, level, stars")
+        result = await stmt.fetch(unit_id, user_id, location)
+    await conn.close()
+    return result
+
+async def get_unit(unit_id: int, user_id: int):
+    conn = await asyncpg.connect(PSQL_CONNECTION_URL)
+    stmt = await conn.prepare("SELECT * FROM units WHERE unit_id=$1 AND user_id=$2")
+    result = await stmt.fetch(unit_id, user_id)
+    return result
+
+async def get_units(user_id: int, location: int):
+    conn = await asyncpg.connect(PSQL_CONNECTION_URL)
+    stmt = await conn.prepare("SELECT * FROM units WHERE user_id=$1 AND location=$2")
+    result = await stmt.fetch(user_id, location)
+    units = {}
+    for unit in result:
+        units[unit.get('unit_id')] = {}
+        for field, value in unit.items():
+            if not field == 'unit_id':
+                units[unit.get('unit_id')][field] = value
+    return units
+
+async def update_user_essence(user_id: int, amount: int):
+    await get_user(user_id)
+    conn = await asyncpg.connect(PSQL_CONNECTION_URL)
+    stmt = await conn.prepare("UPDATE users SET essence=essence + $2 WHERE user_id=$1 RETURNING user_id, essence, currency, stage, elo")
+    result = await stmt.fetch(user_id, amount)
+    await conn.close()
+    return result
+
+async def update_user_currency(user_id: int, amount: int):
+    await get_user(user_id)
+    conn = await asyncpg.connect(PSQL_CONNECTION_URL)
+    stmt = await conn.prepare("UPDATE users SET currency=currency + $2 WHERE user_id=$1 RETURNING user_id, essence, currency, stage, elo")
+    result = await stmt.fetch(user_id, amount)
+    await conn.close()
+    return result
+
+async def update_user_stage(user_id: int, amount: int):
+    await get_user(user_id)
+    conn = await asyncpg.connect(PSQL_CONNECTION_URL)
+    stmt = await conn.prepare("UPDATE users SET stage=stage + $2 WHERE user_id=$1 RETURNING user_id, essence, currency, stage, elo")
+    result = await stmt.fetch(user_id, amount)
+    await conn.close()
+    return result
+
+async def update_user_elo(user_id: int, amount: int):
+    await get_user(user_id)
+    conn = await asyncpg.connect(PSQL_CONNECTION_URL)
+    stmt = await conn.prepare("UPDATE users SET elo=elo + $2 WHERE user_id=$1 RETURNING user_id, essence, currency, stage, elo")
+    result = await stmt.fetch(user_id, amount)
+    await conn.close()
+    return result
+
+async def update_unit_level(unit_id: int, user_id: int, amount: int):
+    unit = await get_unit(unit_id, user_id)
+    if not unit:
+        return None
+    conn = await asyncpg.connect(PSQL_CONNECTION_URL)
+    stmt = await conn.prepare("UPDATE units SET level=level + $3 WHERE unit_id=$1 AND user_id=$2 RETURNING unit_id, user_id, location, level, stars")
+    result = await stmt.fetch(unit_id, user_id, amount)
+    await conn.close()
+    return result
+
+async def update_unit_location(unit_id: int, user_id: int, location: int):
+    unit = await get_unit(unit_id, user_id)
+    if not unit:
+        return None
+    conn = await asyncpg.connect(PSQL_CONNECTION_URL)
+    stmt = await conn.prepare("UPDATE units SET location=$3 WHERE unit_id=$1 AND user_id=$2 RETURNING unit_id, user_id, location, level, stars")
+    result = await stmt.fetch(unit_id, user_id, location)
+    await conn.close()
+    return result
 
 if __name__ == '__main__':
-    asyncio.get_event_loop().run_until_complete(get_user(487291708846112779))
+    print(asyncio.get_event_loop().run_until_complete(update_user_stage(124668192948748288, 1)))
